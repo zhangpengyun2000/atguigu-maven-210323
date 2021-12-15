a8070bc342396fd80c747a1b23149af160f76f0f
zhangpengyun2000 <53132964+zhangpengyun2000@users.noreply.github.com>
2021-12-15 21:13:40 +0800

    Update ABC.java

--- src/main/java/com/atguigu/ssm/entity/ABC.java
+++ src/main/java/com/atguigu/ssm/entity/ABC.java
@@ -3,11 +3,11 @@ package com.atguigu.ssm.entity;
 public class ABC {
     public static void main(String[] args) {
         System.out.println("11111111111master");
-        System.out.println("11111111111DEV");
-        System.out.println("11111111111DEV");
-        System.out.println("11111111111DEV");
-        System.out.println("11111111111DEV");
-        System.out.println("11111111111DEV");
-        System.out.println("11111111111DEV");
+        System.out.println("11111111111DEV111");
+        System.out.println("11111111111DEV111");
+        System.out.println("11111111111DEV111");
+        System.out.println("11111111111DEV111");
+        System.out.println("11111111111DEV111");
+        System.out.println("11111111111DEV111");
     }
 }
