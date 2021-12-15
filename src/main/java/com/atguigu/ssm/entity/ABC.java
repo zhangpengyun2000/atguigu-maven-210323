18f704ab29ee251e2b64a379ab2d44041836a18d
zpy <1257237024@qq.com>
2021-12-15 20:18:38 +0800

    增加了多行DEV

--- src/main/java/com/atguigu/ssm/entity/ABC.java
+++ src/main/java/com/atguigu/ssm/entity/ABC.java
@@ -4,5 +4,10 @@ public class ABC {
     public static void main(String[] args) {
         System.out.println("11111111111master");
         System.out.println("11111111111DEV");
+        System.out.println("11111111111DEV");
+        System.out.println("11111111111DEV");
+        System.out.println("11111111111DEV");
+        System.out.println("11111111111DEV");
+        System.out.println("11111111111DEV");
     }
 }
