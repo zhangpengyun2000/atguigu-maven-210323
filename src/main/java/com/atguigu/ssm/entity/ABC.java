f29c9850119861c0fe4df2665901ec329e269d52
zpy <1257237024@qq.com>
2021-12-15 19:35:22 +0800

    修改第五行代码

--- src/main/java/com/atguigu/ssm/entity/ABC.java
+++ src/main/java/com/atguigu/ssm/entity/ABC.java
@@ -2,6 +2,6 @@ package com.atguigu.ssm.entity;
 
 public class ABC {
     public static void main(String[] args) {
-        System.out.println("11111111111");
+        System.out.println("11111111111master");
     }
 }
